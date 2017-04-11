8c0d48f0da437633a12be4de18fe5f8ff46154a0
deadman96385 <seanhoyt963@gmail.com>
2017-04-11 01:45:12 -0500

    Basic gsm support

--- device.mk
+++ device.mk
@@ -25,4 +25,4 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/lt02ltetmo/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
